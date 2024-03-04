449c7dd6374bb7794d4b919ba34dc9c39188b053
firmianodev <firmiano.dev@gmail.com>
2024-03-04 16:36:17 -0300

    create AuthContext and useAuth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,12 +7,11 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 
 import { Button } from "../components/Button/Button";
-import { useContext } from "react";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
     const navigate = useNavigate();
-    const { user, signInWithGoogle } = useContext(AuthContext);
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
         if (!user) {
